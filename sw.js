c0901ce3b30cd884ed59b5da5bf48a04d1e7a663
Ryan Adi Prayoga <ryanadiprayoga31@gmail.com>
2026-09-22 10:01:45 +0700

    Perbaiki sistem memori PWA

--- sw.js
+++ sw.js
@@ -8,6 +8,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
@@ -16,11 +17,14 @@ self.addEventListener('install', event => {
     );
 });
 
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
     );
 });
